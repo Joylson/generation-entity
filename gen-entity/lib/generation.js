10afd5ef6107a2dd88395edb1eed43b8fd80b899
Joylson <joylsont@gmail.com>
2019-03-28 16:40:05 -0300

    correcao chave composta

--- gen-entity/lib/generation.js
+++ gen-entity/lib/generation.js
@@ -22,33 +22,41 @@ loopStruct = (item) => {
     let struct_coluns = '';
     item.columns.forEach(item2 => {
         let type = checkType(item2);
-        let foreign = createForeign(checkForeign(item2));
+        let lst_foreign = createForeign(checkForeign(item2), item2['']);
         if (checkPrimary(item2)) {
-            struct_primary += '\n\n\t@PrimaryGeneratedColumn(' + (!foreign ? '{name : \'' + item2['column_name'] + '\'}' : '') + ')'
-                + (!foreign ? '\n\t' + literaltoCamelcase(item2['column_name']) + ' : ' + type + ';' : '');
-            if (foreign) {
-                struct_primary += '\n\t@ManyToOne(type => ' + literaltoCamelcase(foreign['table'], true) + ', ' + literaltoCamelcase(foreign['table']) + ' => ' + literaltoCamelcase(foreign['table']) + '.' + literaltoCamelcase(item2['table_name'] + 's') + ')'
-                    + '\n\t@JoinColumn(' + foreign['foreignRef'] + ')'
-                    + '\n\t' + literaltoCamelcase(item2['column_name']) + ' : ' + literaltoCamelcase(foreign['table'], true) + ';'
+            struct_primary += '\n\n\t@PrimaryGeneratedColumn(' + (!lst_foreign ? '{name : \'' + item2['column_name'] + '\'}' : '') + ')'
+                + (!lst_foreign ? '\n\t' + literaltoCamelcase(item2['column_name']) + ' : ' + type + ';' : '');
+            if (lst_foreign) {
+                lst_foreign.forEach(foreign => {
+                    struct_primary += '\n\t@ManyToOne(type => ' + literaltoCamelcase(foreign['table'], true) + ', ' + literaltoCamelcase(foreign['table']) + ' => ' + literaltoCamelcase(foreign['table']) + '.' + lowerCase(literaltoCamelcase(removeId(item2['column_name'] + '_' + item2['table_name']) + 's'), 0) + ')'
+                        + '\n\t@JoinColumn(' + foreign['foreignRef'] + ')'
+                        + '\n\t' + literaltoCamelcase(checkConcatenation(item2['column_name'], foreign['table'], true)) + ' : ' + literaltoCamelcase(foreign['table'], true) + ';' + (lst_foreign.length > 1 ? '\n' : '');
+                });
             }
-        } else if (foreign) {
-            struct_foreign += '\n\n\t@ManyToOne(type => ' + literaltoCamelcase(foreign['table'], true) + ', ' + literaltoCamelcase(foreign['table']) + ' => ' + literaltoCamelcase(foreign['table']) + '.' + literaltoCamelcase(item2['table_name'] + 's')
-                + ', { nullable : ' + (item2['is_nullable'] === 'NO' ? 'false' : 'true') + ' })'
-                + '\n\t@JoinColumn(' + foreign['foreignRef'] + ')'
-                + '\n\t' + literaltoCamelcase(item2['column_name']) + ' : ' + literaltoCamelcase(foreign['table'], true) + ';'
+        } else if (lst_foreign) {
+            lst_foreign.forEach(foreign => {
+                struct_foreign += '\n\n\t@ManyToOne(type => ' + literaltoCamelcase(foreign['table'], true) + ', ' + literaltoCamelcase(foreign['table']) + ' => ' + literaltoCamelcase(foreign['table']) + '.' + lowerCase(literaltoCamelcase(removeId(item2['column_name'] + '_' + item2['table_name']) + 's'), 0)
+                    + ', { nullable : ' + (item2['is_nullable'] === 'NO' ? 'false' : 'true') + ' })'
+                    + '\n\t@JoinColumn(' + foreign['foreignRef'] + ')'
+                    + '\n\t' + literaltoCamelcase(checkConcatenation(item2['column_name'], foreign['table'], true)) + ' : ' + literaltoCamelcase(foreign['table'], true) + ';';
+            });
         } else {
             struct_coluns += (checkPrimary(item2) ? '\n\t@PrimaryGeneratedColumn({name : \'' + item2['column_name'] + '\'})' :
                 '\n\n\t@Column(\'' + checkInrregular(item2['udt_name']) + '\', {name : \'' + item2['column_name'] + '\'' + (item2['length'] === null ? '' : ', length : ' + item2['length']) + ', nullable : ' + (item2['is_nullable'] === 'NO' ? 'false' : 'true') + '})')
-                + '\n\t' + literaltoCamelcase(item2['column_name']) + ' : ' + type + ';'
+                + '\n\t' + literaltoCamelcase(item2['column_name']) + ' : ' + type + ';';
         }
     });
 
     let tables_ref = [];
     item.refConstraints.forEach(item => {
-        if (!checkIncludes(item['table_name'], tables_ref, (obj, res) => obj === res)) {
-            struct_ref_foreign += '\n\n\t@OneToMany(type => ' + literaltoCamelcase(item['table_name'], true) + ', ' + literaltoCamelcase(item['table_name']) + ' => ' + literaltoCamelcase(item['table_name']) + '.' + literaltoCamelcase(item['column_name']) + ' )'
-                + '\n\t' + literaltoCamelcase(item['table_name']) + 's : ' + literaltoCamelcase(item['table_name'], true) + '[];';
-            tables_ref.push(item['table_name']);
+        if (!checkIncludes({ foreing: item['constraint_name'], table: item['table_name'], column: item['column_name'] }, tables_ref, (obj, res) => {
+            if (obj['foreing'] === res['foreing'] && obj['table'] === res['table'] && obj['column'] === res['column']) {
+                return true;
+            }
+        })) {
+            struct_ref_foreign += '\n\n\t@OneToMany(type => ' + literaltoCamelcase(item['table_name'], true) + ', ' + literaltoCamelcase(item['table_name']) + ' => ' + literaltoCamelcase(item['table_name']) + '.' + literaltoCamelcase(checkConcatenation(item['column_name'], item['table_name_foreign'], true)) + ' )'
+                + '\n\t' +  lowerCase(literaltoCamelcase(removeId(item['column_name'] + '_' + item['table_name']) + 's'), 0) + ' : ' + literaltoCamelcase(item['table_name'], true) + '[]; ';
+            tables_ref.push({ foreing: item['constraint_name'], table: item['table_name'], column: item['column_name'] });
         }
     });
 
@@ -90,17 +98,20 @@ checkPrimary = (column) => {
 }
 
 createForeign = (foreign) => {
-    let foreign_ref = '';
     if (foreign.length > 0) {
-        foreign_ref = foreign.length > 1 ? '[' : '';
-        let count = 0;
-        foreign.forEach(item => {
-            if(count > 0) foreign_ref += ', ';
-            foreign_ref += '{ name : \'' + item['column'] + '\', referencedColumnName: \'' + literaltoCamelcase(item['column']) + '\'}';
-            count++;
+        foreign.forEach((item, key) => {
+            let foreign_ref = '';
+            foreign_ref = item.columns.length > 1 ? '[' : '';
+            let count = 0;
+            item.columns.forEach(item2 => {
+                if (count > 0) foreign_ref += ', ';
+                foreign_ref += '{ name : \'' + item2['columnName'] + '\', referencedColumnName: \'' + literaltoCamelcase(item2['columnRef']) + '\'}';
+                count++;
+            });
+            foreign_ref += item.columns.length > 1 ? ']' : '';
+            foreign[key]['foreignRef'] = foreign_ref;
         });
-        foreign_ref += foreign.length > 1 ? ']' : '';
-        return { table: foreign[0]['table'], column: foreign[0]['column'], foreignRef: foreign_ref }
+        return foreign;
     } else {
         return null;
     }
@@ -109,7 +120,14 @@ createForeign = (foreign) => {
 checkForeign = (column) => {
     let foreignKey = [];
     column.constraints.forEach(item => {
-        if (item['constraint_type'] === 'FOREIGN KEY') foreignKey.push({ table: item['table_name_foreign'], column: item['column_name_foreign'] });
+        if (item['constraint_type'] === 'FOREIGN KEY' && !checkIncludes(item['constraint_name'], foreignKey, (obj, res, key) => {
+            if (obj === res['fkName']) {
+                foreignKey[key].columns.push({columnName: item['column_name'], columnRef : item['column_name_foreign']});
+                return true;
+            }
+        })) {
+            foreignKey.push({ fkName: item['constraint_name'], table: item['table_name_foreign'], columns: [{columnName: item['column_name'], columnRef : item['column_name_foreign']}] });
+        }
     });
     return foreignKey;
 }
@@ -117,12 +135,42 @@ checkForeign = (column) => {
 literaltoCamelcase = (text, first) => {
     let text_return = '';
     for (var i = 0; i < text.length; i++) {
-        if (first && i === 0) {
+        if (first && i === 0 && text.charAt(i) !== '_') {
             text_return += text.charAt(i).toUpperCase();
         } else if (text.charAt(i) !== '_') {
             text_return += text.charAt(i);
         } else {
             text_return += text.charAt(++i).toUpperCase();
+        }
+    }
+    return text_return;
+};
+
+checkConcatenation = (text_base, text_conc, base) => {
+    if (!literaltoCamelcase(text_base).toUpperCase().includes(literaltoCamelcase(text_conc).toUpperCase())) {
+        return text_base +  '_' + text_conc ;
+    }
+    return base ? text_base : text_conc;
+};
+
+removeId = (text) =>{
+    if(text.length > 2){
+        if((text.charAt(0) + text.charAt(1)).toUpperCase() === 'ID'){
+            return text.slice(2);
+        }else if((text.charAt(text.length - 2) + text.charAt(text.length - 1)).toUpperCase() === 'ID') {
+            return text.slice(0, text.length - 2)
+        }
+        return text;
+    }
+}
+
+lowerCase = (text, index) => {
+    let text_return = '';
+    for(let i = 0; i < text.length ; i++){
+        if(i === index){
+            text_return += text.charAt(i).toLowerCase();
+        }else{
+            text_return += text.charAt(i);
         }
     }
     return text_return;
@@ -158,8 +206,8 @@ checkImportForeign = (table) => {
 
 checkIncludes = (obj, list, validate) => {
     let test = false
-    list.forEach(res => {
-        if (validate(obj, res)) test = true;
+    list.forEach((res, key) => {
+        if (validate(obj, res, key)) test = true;
     })
     return test;
 }
